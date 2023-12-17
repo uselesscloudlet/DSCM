7b71e8b160949f0602fb98a7753fea7eec92da35
Dmitrii <uselesscloud@hotmail.com>
2023-12-17 14:53:52 +0300

    brand new solution

--- server/server.py
+++ server/server.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import pandas as pd
@@ -18,6 +19,12 @@ class Number(BaseModel):
     number: int
 
 
+def parse_csv(df):
+    res = df.to_json(orient="records")
+    parsed = json.loads(res)
+    return parsed
+
+
 @app.post("/check_number/")
 async def check_number(request: Number):
     number = request.number
@@ -34,19 +41,36 @@ async def check_number(request: Number):
             status_code=406, detail=f"Number {number} already is in database"
         )
 
-    sql_query = f"SELECT * FROM Numbers WHERE number = {number + 1}"
+    sql_query = f"SELECT * FROM Numbers WHERE number = {number + 1};"
     result = pd.read_sql(sql_query, postgres_connection)
     if len(result) > 0:
         raise HTTPException(
             status_code=406, detail=f"Number {number} + 1 already is in database"
         )
 
-    postgres_connection.execute(text(f"INSERT INTO Numbers (number) VALUES ({number})"))
+    postgres_connection.execute(
+        text(f"INSERT INTO Numbers (number) VALUES ({number});")
+    )
 
-    print(pd.read_sql("SELECT * FROM Numbers", postgres_connection))
-    postgres_connection.commit()
     postgres_connection.close()
 
     return JSONResponse(
         status_code=status.HTTP_200_OK, content=f"Added {number} in database"
     )
+
+
+@app.post("/clear_numbers/")
+async def clear_numbers():
+    postgres_connection = ENGINE.connect()
+    postgres_connection.execute(text("DELETE FROM Numbers;"))
+    postgres_connection.close()
+    return JSONResponse(status_code=status.HTTP_200_OK, content="Database is clear now")
+
+
+@app.get("/get_data/")
+async def get_data():
+    postgres_connection = ENGINE.connect()
+    data = pd.read_sql("SELECT * FROM Numbers", postgres_connection)
+    postgres_connection.close()
+    return JSONResponse(status_code=status.HTTP_200_OK, content=parse_csv(data))
+
